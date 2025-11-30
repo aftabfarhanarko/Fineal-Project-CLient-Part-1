dab16c321a010eec601dad69de9a449d108ecf91
Aftab Farhan <aftabfarhan324@gmail.com>
2025-11-30 21:30:07 +0600

    user delete api creat and ui ubdeat

--- src/pages/Dashbord/userManage/UserManage.jsx
+++ src/pages/Dashbord/userManage/UserManage.jsx
@@ -3,12 +3,15 @@ import useAxiosSecoir from "../../../Hook/useAxiosSecoir";
 import { useQuery } from "@tanstack/react-query";
 import { FaUserShield, FaUserTimes } from "react-icons/fa";
 import Swal from "sweetalert2";
+import Loding from "../../../Shared/Loding";
+import { MdDeleteOutline, MdOutlineDeleteOutline } from "react-icons/md";
+import { toast } from "sonner";
 
 const UserManage = () => {
   const axiosSecoir = useAxiosSecoir();
   const [searchText, setSearchText] = useState("");
 
-  const { refetch, data } = useQuery({
+  const { refetch, data, isLoading } = useQuery({
     queryKey: ["users", searchText],
     queryFn: async () => {
       const res = await axiosSecoir.get(`users?searchText=${searchText}`);
@@ -26,7 +29,6 @@ const UserManage = () => {
     Swal.fire({
       icon: "success",
       title: `Admin Added Successfully ${user.displayName}`,
-      // text: "The rider has been successfully added to the system. Admin will review and verify the rider details shortly.",
 
       confirmButtonText: "OK",
       customClass: {
@@ -80,6 +82,57 @@ const UserManage = () => {
     });
     //
   };
+  const handelDeletUser = (id) => {
+    console.log(id);
+
+    Swal.fire({
+      title: "Confirm User Deletion",
+      text: `Are you sure you want to delete this User? This action cannot be undone.`,
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: "Yes, Delete",
+      cancelButtonText: "No, Cancel",
+
+      customClass: {
+        popup: "rounded-2xl shadow-xl",
+        title: "text-lg font-semibold text-gray-800",
+        htmlContainer: "text-gray-600",
+        actions: "flex gap-3 justify-end",
+        confirmButton:
+          "bg-blue-600 hover:bg-blue-700 text-white font-semibold px-6 py-2 rounded-xl",
+        cancelButton:
+          "bg-red-500 hover:bg-red-600 text-white font-semibold px-6 py-2 rounded-xl",
+      },
+
+      buttonsStyling: false,
+      backdrop: `rgba(0,0,0,0.45)`,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecoir.delete(`user/${id}`).then((res) => {
+          refetch();
+          console.log(res.data);
+        });
+        Swal.fire({
+          icon: "success",
+          title: "User Deleted Successfully ",
+          text: "This User has been Deletd Proprely .",
+          confirmButtonText: "OK",
+          customClass: {
+            popup: "rounded-2xl shadow-lg",
+            title: "text-lg font-bold text-green-700",
+            htmlContainer: "text-gray-700",
+            confirmButton:
+              "bg-green-600 hover:bg-green-700 text-white font-semibold px-6 py-2 rounded-xl",
+          },
+          buttonsStyling: false,
+        });
+      }
+    });
+  };
+
+  if (isLoading) {
+    return <Loding></Loding>;
+  }
 
   return (
     <div className=" py-6 md:py-10 px-2 md:px-15">
@@ -208,15 +261,15 @@ const UserManage = () => {
                   {/* Actions */}
                   <td className="p-4">
                     <div className="flex items-center gap-3">
-                      <button className="px-4 py-1.5 rounded-lg bg-white text-green-600 border border-green-300 flex items-center gap-2 font-medium hover:bg-green-50 hover:shadow-sm transition">
+                      {/* <button className="px-4 py-1.5 rounded-lg bg-white text-green-600 border border-green-300 flex items-center gap-2 font-medium hover:bg-green-50 hover:shadow-sm transition">
                         Approved
-                      </button>
+                      </button> */}
 
                       <button
-                        onClick={() => handelDelet(item._id)}
-                        className="px-4 py-1.5 rounded-lg bg-white text-red-600 border border-red-300 flex items-center gap-2 font-medium hover:bg-red-50 hover:shadow-sm transition"
+                        onClick={() => handelDeletUser(item._id)}
+                        className="px-4 py-1.5 rounded-lg bg-white text-red-600 border border-red-300 flex items-center gap-2 font-medium hover:bg-red-50 hover:shadow-sm transition "
                       >
-                        Delete
+                        Delete <MdOutlineDeleteOutline size={20} />
                       </button>
                     </div>
                   </td>
